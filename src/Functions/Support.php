e5d46bc2156267e28e399668ccf3d7c05759f374
Kevin Nowaczyk <beakerboy99@yahoo.com>
2017-05-31 07:16:45 -0500

    Update Support.php

--- src/Functions/Support.php
+++ src/Functions/Support.php
@@ -128,7 +128,7 @@ class Support
         // Upper Triangle
         $B_array = [];
         for ($i=0; $i<=$n; $i++) {
-            for ($j=0; $j<$n; $j++) {
+            for ($j=0; $j<=$n; $j++) {
                 if ($i == 0) {
                     $B_array[$i][$j] = 1;
                 } elseif ($i > $j) {
@@ -144,7 +144,7 @@ class Support
         // Lower Triangle
         $C_array = [];
         for ($i=0; $i<=$n; $i++) {
-            for ($j=0; $j<$n; $j++) {
+            for ($j=0; $j<=$n; $j++) {
                 if ($i == 0 && $j == 0) {
                     $C_array[$i][$j] = .5;
                 } elseif ($i < $j) {
