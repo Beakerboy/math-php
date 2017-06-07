8ec8e78bdc5a669f7735301e9b8eb12a54f20bbd
Kevin Nowaczyk <beakerboy99@yahoo.com>
2017-06-07 06:56:29 -0400

    Update Boolean.php

--- src/Functions/Boolean.php
+++ src/Functions/Boolean.php
@@ -32,7 +32,7 @@ class Boolean
     {
         if (is_int($a + $b)) {
             $sum = $a + $b;
-            return ['overflow'=> !($a > 0 && $b > 0) && $sum > 0, 'value' => $sum];
+            return ['overflow'=> !($a > 0 && $b > 0 || $sum < 0), 'value' => $sum];
         } elseif ($a > 0 && $b > 0) {
             $c = $a - \PHP_INT_MAX + $b - 1 + \PHP_INT_MIN;
             return ['overflow'=> false, 'value' => $c];
