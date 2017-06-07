6c65163882450a2c9b4623a9464f2df35e7ee95b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2017-06-07 06:04:46 -0400

    Update BooleanTest.php

--- tests/Functions/BooleanTest.php
+++ tests/Functions/BooleanTest.php
@@ -34,6 +34,9 @@ class BooleanTest extends \PHPUnit_Framework_TestCase
             [
                 \PHP_INT_MIN, \PHP_INT_MIN, [true, 0]
             ],
+            [
+                \PHP_INT_MIN, \PHP_INT_MAX, [false, -1]
+            ],
         ];
     }
 }
