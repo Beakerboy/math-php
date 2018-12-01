9a0af39683ea54208749386d08eaafdd66fcb0dc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2018-12-01 11:17:03 -0500

    Update DistanceTest.php

--- tests/Statistics/DistanceTest.php
+++ tests/Statistics/DistanceTest.php
@@ -299,7 +299,7 @@ class DistanceTest extends \PHPUnit\Framework\TestCase
         for ($i=0; $i<10; $i++) {
             $data_matrix = new Matrix($data);
             $x = new Matrix($data_matrix->getColumn($i));
-            $calc = Distance::Mahalanobis($x, $data, $y[$i]);
+            $calc = Distance::Mahalanobis($x, $data_matrix, $y[$i]);
             $this->assertEquals($distances[$i], $calc, '', 0.0001);
         }
     }
