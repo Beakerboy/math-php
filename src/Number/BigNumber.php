e42db37f41e4b609be4a362e7843750308ce7d3d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2018-12-08 22:28:16 -0500

    Create BigNumber.php

---
+++ src/Number/BigNumber.php
@@ -0,0 +1,174 @@
+<?php
+namespace MathPHP\Number;
+
+use MathPHP\Exception;
+use MathPHP\Functions\Special;
+
+/**
+ * Big Numbers
+ *
+ * The PHP BCMath library can perform arithmatic on numbers that are larger than the native
+ * number types.
+ */
+class BigNumber implements ObjectArithmetic
+{
+    /**
+     * The number.
+     * @var number
+     */
+    protected $number;
+    
+    /**
+     * Constructor
+     *
+     * @param number $number
+     */
+    public function __construct(string $number)
+    {
+        $this->number = $number;
+    }
+    
+    /**
+     * String representation of a complex number
+     * a + bi, a - bi, etc.
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $number;
+    }
+    
+    /**************************************************************************
+     * UNARY FUNCTIONS
+     **************************************************************************/
+
+    /**
+     * The absolute value of a BigNumber
+     *
+     * @return BigNumber
+     */
+    public function abs(): BigNumber
+    {
+        return bccomp($this->number, '0') == -1 ? new BigNumber(bcmul($this->number, '-1')) : new BigNumber($this->number);
+    }
+    
+    /**
+     * The square root of a BigNumber
+     *
+     * @return BigNumber (positive root)
+     */
+    public function sqrt(): BigNumber
+    {
+        return new BigNumber(bcsqrt($this->number));
+    }
+
+    /**************************************************************************
+     * BINARY FUNCTIONS
+     **************************************************************************/
+
+    /**
+     * Addition
+     *
+     * @param mixed $input
+     *
+     * @return BigNumber
+     *
+     * @throws Exception\IncorrectTypeException if the argument is not string or BigNumber.
+     */
+    public function add($input): BigNumber
+    {
+        if (preg_match('/^0$|^[-]?[1-9.][0-9.]$/', $input)) {
+            $n = $input;
+        } elseif ($input instanceof BigNumber) {
+            $n = $input->ToString();
+        } else {
+            throw new Exception\IncorrectTypeException('Argument must be string or BigNumber');
+        }
+
+        return new BigNumber(bcadd($this->number, $n));
+    }
+
+    /**
+     * Subtraction
+     *
+     * (a + bi) - (c + di) = (a - c) + (b - d)i
+     *
+     * @param mixed $input
+     *
+     * @return BigNumber
+     *
+     * @throws Exception\IncorrectTypeException if the argument is not string or BigNumber.
+     */
+    public function subtract($c): Complex
+    {
+        if (preg_match('/^0$|^[-]?[1-9.][0-9.]$/', $input)) {
+            $n = $input;
+        } elseif ($input instanceof BigNumber) {
+            $n = $input->ToString();
+        } else {
+            throw new Exception\IncorrectTypeException('Argument must be string or BigNumber');
+        }
+
+        return new BigNumber(bcsub($this->number, $n));
+    }
+
+    /**
+     * Multiplication
+     *
+     * @param mixed $input
+     *
+     * @return BigNumber
+     *
+     * @throws Exception\IncorrectTypeException if the argument is not string or BigNumber.
+     */
+    public function multiply($input): BigNumber
+    {
+        if (preg_match('/^0$|^[-]?[1-9.][0-9.]$/', $input)) {
+            $n = $input;
+        } elseif ($input instanceof BigNumber) {
+            $n = $input->ToString();
+        } else {
+            throw new Exception\IncorrectTypeException('Argument must be string or BigNumber');
+        }
+        return new BigNumber(bcmul($this->number, $n));
+    }
+
+    /**
+     * Division
+     *
+     * @param mixed $input
+     *
+     * @return BigNumber
+     *
+     * @throws Exception\IncorrectTypeException if the argument is not string or BigNumber.
+     */
+    public function divide($input): BigNumber
+    {
+        if (preg_match('/^0$|^[-]?[1-9.][0-9.]$/', $input)) {
+            $n = $input
+        } elseif ($input instanceof BigNumber) {
+            $$n = $input->ToString();
+        } else {
+            throw new Exception\IncorrectTypeException('Argument must be real or complex number');
+        }
+        return new BigNumber(bcdiv($this->number, $n));
+    }
+    
+    /**************************************************************************
+     * COMPARISON FUNCTIONS
+     **************************************************************************/
+
+    /**
+     * Test for equality
+     *
+     *
+     * @param BigNumber $input
+     *
+     * @return bool
+     */
+    public function equals(BigNumber $input): bool
+    {
+        return bccomp($this->number, $input->ToString()) === 0;
+    }
+}
