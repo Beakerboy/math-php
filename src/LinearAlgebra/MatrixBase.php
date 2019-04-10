9e5e175d216f575a534b213bc67d37c784f3aff0
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-04-10 09:44:56 -0400

    Adding base properties

--- src/LinearAlgebra/MatrixBase.php
+++ src/LinearAlgebra/MatrixBase.php
@@ -1,8 +1,18 @@
 <?php
 
 namespace MathPHP\LinearAlgebra;
-
-abstract class MatrixBase
+**
+ * m x n Matrix
+ */
+abstract class MatrixBase implements \ArrayAccess, \JsonSerializable
 {
+    /** @var int Number of rows */
+    protected $m;
+
+    /** @var int Number of columns */
+    protected $n;
+
+    /** @var array Matrix array of arrays */
+    protected $A;
 
 }
