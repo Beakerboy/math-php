f92a6d5081bba7b1478dafe3f30dae499cbcf211
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-04-10 09:56:24 -0400

    Added interface methods

--- src/LinearAlgebra/MatrixBase.php
+++ src/LinearAlgebra/MatrixBase.php
@@ -1,8 +1,15 @@
 <?php
 
 namespace MathPHP\LinearAlgebra;
+
+use MathPHP\Exception;
+
 /**
  * m x n Matrix
+ *
+ * This abstract class contains properties and methods that are shared by matricies
+ * regardless of the type of data that they contain.
+ *
  */
 abstract class MatrixBase implements \ArrayAccess, \JsonSerializable
 {
@@ -14,4 +21,146 @@ abstract class MatrixBase implements \ArrayAccess, \JsonSerializable
 
     /** @var array Matrix array of arrays */
     protected $A;
+    
+    /**************************************************************************
+     * BASIC MATRIX GETTERS
+     *  - getMatrix
+     *  - getM
+     *  - getN
+     *  - getRow
+     *  - getColumn
+     **************************************************************************/
+    /**
+     * Get matrix
+     * @return array of arrays
+     */
+    public function getMatrix(): array
+    {
+        return $this->A;
+    }
+    /**
+     * Get row count (m)
+     * @return int number of rows
+     */
+    public function getM(): int
+    {
+        return $this->m;
+    }
+    /**
+     * Get column count (n)
+     * @return int number of columns
+     */
+    public function getN(): int
+    {
+        return $this->n;
+    }
+    /**
+     * Get single row from the matrix
+     *
+     * @param  int    $i row index (from 0 to m - 1)
+     * @return array
+     *
+     * @throws Exception\MatrixException if row i does not exist
+     */
+    public function getRow(int $i): array
+    {
+        if ($i >= $this->m) {
+            throw new Exception\MatrixException("Row $i does not exist");
+        }
+        return $this->A[$i];
+    }
+    /**
+     * Get single column from the matrix
+     *
+     * @param  int   $j column index (from 0 to n - 1)
+     * @return array
+     *
+     * @throws Exception\MatrixException if column j does not exist
+     */
+    public function getColumn(int $j): array
+    {
+        if ($j >= $this->n) {
+            throw new Exception\MatrixException("Column $j does not exist");
+        }
+        return array_column($this->A, $j);
+    }
+    /**
+     * Get a specific value at row i, column j
+     *
+     * @param  int    $i row index
+     * @param  int    $j column index
+     * @return number
+     *
+     * @throws Exception\MatrixException if row i or column j does not exist
+     */
+    public function get(int $i, int $j)
+    {
+        if ($i >= $this->m) {
+            throw new Exception\MatrixException("Row $i does not exist");
+        }
+        if ($j >= $this->n) {
+            throw new Exception\MatrixException("Column $j does not exist");
+        }
+        return $this->A[$i][$j];
+    }
+    
+    /**************************************************************************
+     * ArrayAccess INTERFACE
+     **************************************************************************/
+    /**
+     * @param mixed $i
+     * @return bool
+     */
+    public function offsetExists($i): bool
+    {
+        return isset($this->A[$i]);
+    }
+
+    /**
+     * @param mixed $i
+     * @return mixed
+     */
+    public function offsetGet($i)
+    {
+        return $this->A[$i];
+    }
+
+    /**
+     * @param  mixed $i
+     * @param  mixed $value
+     * @throws Exception\MatrixException
+     */
+    public function offsetSet($i, $value)
+    {
+        throw new Exception\MatrixException('Matrix class does not allow setting values');
+    }
+
+    /**
+     * @param  mixed $i
+     * @throws Exception\MatrixException
+     */
+    public function offsetUnset($i)
+    {
+        throw new Exception\MatrixException('Matrix class does not allow unsetting values');
+    }
+
+    /**************************************************************************
+     * JsonSerializable INTERFACE
+     **************************************************************************/
+    /**
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        return $this->A;
+    }
+
+    /**
+     * Get the type of objects that are stored in the matrix
+     *
+     * @return string The class of the objects
+     */
+    abstract public function getObjectType(): string
+    {
+    }
 }
