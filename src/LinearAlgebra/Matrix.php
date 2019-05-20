d162c2d58a406041fdfbfb31a8be7cee32139da7
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-05-20 12:11:20 -0400

    Update Matrix.php

--- src/LinearAlgebra/Matrix.php
+++ src/LinearAlgebra/Matrix.php
@@ -3692,10 +3692,10 @@ class Matrix implements \ArrayAccess, \JsonSerializable
             throw new Exception\MatrixException('Eigenvalues can only be calculated on square matrices');
         }
         if ($method == Eigenvalue::CLOSED_FORM_POLYNOMIAL_ROOT_METHOD || ($method === null && $this->m < 5)) {
-            return Eigenvalue(Eigenvalue::CLOSED_FORM_POLYNOMIAL_ROOT_METHOD);
+            return Eigenvalue::closedFormPolynomialRootMethod($this);
         }
         if ($method == Eigenvalue::JK_METHOD || ($method === null && $this->isSymmetric())) {
-            return Eigenvalue(Eigenvalue::JK_METHOD);
+            return Eigenvalue::JKMethod($this);
         }
         return Eigenvalue::$method($this);
     }
@@ -3722,10 +3722,10 @@ class Matrix implements \ArrayAccess, \JsonSerializable
             throw new Exception\MatrixException('Eigenvectors can only be calculated on square matrices');
         }
         if ($method == Eigenvalue::CLOSED_FORM_POLYNOMIAL_ROOT_METHOD || ($method === null && $this->m < 5)) {
-            return Eigenvector::eigenvectors($this, Eigenvalue::$method($this));
+            return Eigenvector::eigenvectors($this, Eigenvalue::closedFormPolynomialRootMethod($this));
         }
         if ($method == Eigenvalue::JK_METHOD || ($method === null && $this->isSymmetric())) {
-            return Eigenvector::eigenvectors($this, Eigenvalue::$method($this));
+            return Eigenvector::eigenvectors($this, Eigenvalue::JKMethod($this));
         }
         return Eigenvector::eigenvectors($this, Eigenvalue::$method($this));
     }
