c8b06e2f71e88b79ee423be7c224f978deb5323e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-08-08 14:13:20 -0400

    Update GivensMatrixTest.php

--- tests/LinearAlgebra/GivensMatrixTest.php
+++ tests/LinearAlgebra/GivensMatrixTest.php
@@ -12,4 +12,26 @@ class GivensMatrixTest extends \PHPUnit\Framework\TestCase
         // When
         $matrix = MatrixFactory::givens(2, 3, \M_PI, 2);
     }
+
+    public function testGivensMatrix($m, $n, $angle, $size, $expected)
+    {
+    }
+
+    public function dataProviderForTestGivensMatrix()
+    {
+        return [
+            [
+                0, 1, \M_PI, 2, 
+                [[-1, 0],
+                 [0, -1]]
+            ],
+            [
+                1, 2, \M_PI / 4, 3, 
+                [[\M_SQRT1_2, 0,  -1 * \M_SQRT1_2],
+                 [0,          1,  0],
+                 [\M_SQRT1_2, 0, \M_SQRT1_2]],
+            ],
+        ];
+    }
+    
 }
