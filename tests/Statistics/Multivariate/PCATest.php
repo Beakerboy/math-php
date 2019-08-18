d816d41fea8c95ec096dc68833d5d1ba74e8f891
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-08-18 00:34:54 -0400

    Update PCATest.php

--- tests/Statistics/Multivariate/PCATest.php
+++ tests/Statistics/Multivariate/PCATest.php
@@ -186,8 +186,8 @@ class PCATest extends \PHPUnit\Framework\TestCase
      */
     public function testNewDataScores()
     {
-        $expected = [[0.1257286, 7.899684, 2.327884, -0.366373, 1.284736, -5.869623, -3.59103, -1.97999, 1.738207]];
-        $sign_adjusted = MatrixFactory::create($expected)->multiply($this->signature);
+        $expected = MatrixFactory::create([[0.1257286, 7.899684, 2.327884, -0.366373, 1.284736, -5.869623, -3.59103, -1.97999, 1.738207]]);;
+        $sign_adjusted = $expected->multiply($this->signature);
         $scores = $this->pca->getScores(MatrixFactory::create([[1,2,3,4,5,6,7,8,9]]));
         $this->assertTrue($sign_adjusted->isEqual($scores));
     }
