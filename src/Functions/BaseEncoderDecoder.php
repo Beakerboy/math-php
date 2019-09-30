bbda7d67de0bb10a576f794f46bfbb14d52f75c5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-09-30 10:37:15 -0400

    Create BaseEncoderDecoder.php

---
+++ src/Functions/BaseEncoderDecoder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace MathPHP\Functions;
+
+use MathPHP\Number\ArbitraryInteger;
+
+/**
+ * Utility functions to manipulate numerical strings with non-standard bases and alphabets
+ */
+class BaseEncoderDecoder
+{
+    public function toBase(string $number, int $fromBase, int $toBase, string $fromAlphabet, string $toAlphabet): string
+    {
+    
+    }
+
+    public function createArbitraryInteger(): ArbitraryInteger
+    {
+    }
+
+    
+}
