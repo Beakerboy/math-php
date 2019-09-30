d30ecbad3b4749a82b86401c802a5eceedbda912
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-09-30 16:02:55 -0400

    Update BaseEncoderDecoder.php

--- src/Functions/BaseEncoderDecoder.php
+++ src/Functions/BaseEncoderDecoder.php
@@ -9,6 +9,15 @@ use MathPHP\Number\ArbitraryInteger;
  */
 class BaseEncoderDecoder
 {
+    /** string alphabet of base 64 numbers */
+    const RFC3548_BASE64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+
+    /** string alphabet of file safe base 64 numbers */
+    const RFC3548_BASE64_FILE_SAFE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+
+    /** string alphabet of base 32 numbers */
+    const RFC3548_BASE32 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
+
     public function toBase(string $number, int $fromBase, int $toBase, string $fromAlphabet, string $toAlphabet): string
     {
     }
