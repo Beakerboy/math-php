8d4f189e2dbe2618effd18e09adbefc70a91a707
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-09-30 22:29:41 -0400

    Update ArbitraryInteger.php

--- src/Number/ArbitraryInteger.php
+++ src/Number/ArbitraryInteger.php
@@ -166,30 +166,6 @@ class ArbitraryInteger implements ObjectArithmetic
     }
 
     /**
-     * Get the default alphabet for a given number base
-     *
-     * @param int $base
-     * @return string
-     */
-    protected static function getDefaultAlphabet(int $base): string
-    {
-        switch ($base) {
-            case 2:
-            case 8:
-            case 10:
-                $offset = '0';
-                break;
-            case 16:
-                $offset = '0123456789abcdef';
-                break;
-            default:
-                $offset = chr(0);
-                break;
-        }
-        return $offset;
-    }
-
-    /**
      * Return the number as a binary string
      * @return string
      */
@@ -201,47 +177,6 @@ class ArbitraryInteger implements ObjectArithmetic
     public function getPositive(): bool
     {
         return $this->positive;
-    }
-
-    /**
-     * Convert to an arbitrary base and alphabet
-     *
-     * @param int $base
-     * @param string $alphabet
-     *
-     * @return string
-     */
-    public function toBase(int $base, $alphabet = null): string
-    {
-        if ($base > 256) {
-            throw new Exception\BadParameterException("Number base cannot be greater than 256.");
-        }
-        if ($alphabet === null) {
-            $alphabet = self::getDefaultAlphabet($base);
-        }
-        $base_256 = $this->base256;
-        $result = '';
-        while ($base_256 !== '') {
-            $carry = 0;
-            $next_int = $base_256;
-            $len = strlen($base_256);
-            $base_256 = '';
-            for ($i = 0; $i < $len; $i++) {
-                $chr = ord($next_int[$i]);
-                $int = intdiv($chr + 256 * $carry, $base);
-                $carry = ($chr + 256 * $carry) % $base;
-                // or just trim off all leading chr(0)s
-                if ($base_256 !== '' || $int > 0) {
-                    $base_256 .= chr($int);
-                }
-            }
-            if (strlen($alphabet) == 1) {
-                $result = chr(ord($alphabet) + $carry) . $result;
-            } else {
-                $result = $alphabet[$carry] . $result;
-            }
-        }
-        return $result;
     }
 
     protected function setVariables(string $value, bool $positive)
