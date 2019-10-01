7d7fcea0107db86e4933e818ece0ac3eed6e253f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-10-01 08:55:26 -0400

    Update ArbitraryInteger.php

--- src/Number/ArbitraryInteger.php
+++ src/Number/ArbitraryInteger.php
@@ -319,7 +319,7 @@ class ArbitraryInteger implements ObjectArithmetic
         return $result;
     }
 
-        /**
+    /**
      * Integer Division
      */
     public function intdiv($divisor): array
@@ -378,6 +378,23 @@ class ArbitraryInteger implements ObjectArithmetic
         return [$int, $mod];
     }
 
+    /**************************************************************************
+     * BINARY FUNCTIONS
+     **************************************************************************/
+
+    /**
+     * Multiply by -1
+     *
+     * If $this is zero, then do nothing
+     */
+    public function negate(): ArbitraryInteger
+    {
+        $result = new ArbitraryInteger(0);
+        $base256 = $this->base256
+        $result->setVariables($base256, $base256 == chr(0) ? true : !$positive);
+        return $result;
+    }
+
     /**
      * Integer Square Root
      *
@@ -422,6 +439,37 @@ class ArbitraryInteger implements ObjectArithmetic
             $result = $result->multiply($i_obj);
         }
         return $result;
+    }
+
+    /**
+     * Ackermann Function
+     * A well known highly recursive function which produces very large numbers
+     *
+     * https://en.wikipedia.org/wiki/Ackermann_function
+     *
+     * @param $m
+     * @param $n
+     * @return ArbitraryInteger
+     */
+    public static function ackermann($m, $n): ArbitraryInteger
+    {
+        $m = self::prepareParameter($m);
+        $n = self::prepareParameter($n);
+        if ($m->equals(0)) {
+            return $n->add(1);
+        } elseif ($m->equals(1)) {
+            return $n->add(2);
+        } elseif ($m->equals(2)) {
+            return $n->leftShift(1)->add(3);
+        } elseif ($m->equals(3)) {
+            $one = new ArbitraryInteger(1);
+            // 2^(n+3) - 3
+            return $one->leftShift($n->add(3))->subtract(3);
+        } elseif ($n->equals(0)) {
+            return ArbitraryInteger::ackermann($m->subtract(1), 1);
+        } else {
+            return ArbitraryInteger::ackermann($m->subtract(1), ArbitraryInteger::ackermann($m, $n->subtract(1)));
+        }
     }
 
     /**
@@ -521,37 +569,6 @@ class ArbitraryInteger implements ObjectArithmetic
             }
             // Must be equal
             return false;
-        }
-    }
-
-    /**
-     * Ackermann Function
-     * A well known highly recursive function which produces very large numbers
-     *
-     * https://en.wikipedia.org/wiki/Ackermann_function
-     *
-     * @param $m
-     * @param $n
-     * @return ArbitraryInteger
-     */
-    public static function ackermann($m, $n): ArbitraryInteger
-    {
-        $m = self::prepareParameter($m);
-        $n = self::prepareParameter($n);
-        if ($m->equals(0)) {
-            return $n->add(1);
-        } elseif ($m->equals(1)) {
-            return $n->add(2);
-        } elseif ($m->equals(2)) {
-            return $n->leftShift(1)->add(3);
-        } elseif ($m->equals(3)) {
-            $one = new ArbitraryInteger(1);
-            // 2^(n+3) - 3
-            return $one->leftShift($n->add(3))->subtract(3);
-        } elseif ($n->equals(0)) {
-            return ArbitraryInteger::ackermann($m->subtract(1), 1);
-        } else {
-            return ArbitraryInteger::ackermann($m->subtract(1), ArbitraryInteger::ackermann($m, $n->subtract(1)));
         }
     }
 }
