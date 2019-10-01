a188be32fc099407d81e9269b495ba66aeca52cd
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-10-01 09:07:06 -0400

    Create BaseEncoderDecoderTest.php

---
+++ tests/Functions/BaseEncoderDecoderTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MathPHP\Tests\Functions
+
+use MathPHP\Functions\BaseEncoderDecoder;
+
+class BaseEncoderDecoderTest extends \PHPUnit\Framework\TestCase
+{
+
+    /**
+     * @test     toBase throws an exception when base>256
+     * @throws   BadParameterException
+     */
+    public function testInvalidToBaseException()
+    {
+        // Given
+        $base = 300;
+        $int =  new ArbitraryInteger('123456');
+        // Then
+        $this->expectException(Exception\BadParameterException::class);
+        // When
+        $string =  BaseEncoderDecoder::toBase($base);
+    }
+}
