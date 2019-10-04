5de676c77d62e5156069b244e38dcc40a897a2c1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-10-04 12:30:46 -0400

    Update ArbitraryInteger.php

--- src/Number/ArbitraryInteger.php
+++ src/Number/ArbitraryInteger.php
@@ -198,6 +198,61 @@ class ArbitraryInteger implements ObjectArithmetic
     }
 
     /**************************************************************************
+     * UNARY FUNCTIONS
+     **************************************************************************/
+
+    /**
+     * Multiply by -1
+     *
+     * If $this is zero, then do nothing
+     */
+    public function negate(): ArbitraryInteger
+    {
+        $result = new ArbitraryInteger(0);
+        $base256 = $this->base256;
+        $result->setVariables($base256, $base256 == chr(0) ? true : !$this->positive);
+        return $result;
+    }
+
+    /**
+     * Integer Square Root
+     *
+     * Calculate the largest integer less than the square root of an integer.
+     * https://en.wikipedia.org/wiki/Integer_square_root
+     */
+    public function isqrt(): ArbitraryInteger
+    {
+        $length = strlen($this->base256);
+        // Start close to the value, at a number around half the digits.
+        $X = self::fromBinary(substr($this->base256, 0, intdiv($length, 2) + 1), true);
+        $lastX = $X;
+        $converge = false;
+        while (!$converge) {
+            list($NX, $mod) = $this->intdiv($X);
+            list($X, $mod) = $X->add($NX)->intdiv(2);
+            if ($X->equals($lastX) || $X->equals($lastX->add(1))) {
+                $converge = true;
+            } else {
+                $lastX = $X;
+            }
+        }
+        return $lastX;
+    }
+
+    /**
+     * Absolute Value
+     *
+     * @return ArbitraryInteger
+     */
+    public function abs(): ArbitraryInteger
+    {
+        $result = new ArbitraryInteger(0);
+        $base256 = $this->base256;
+        $result->setVariables($base256, true);
+        return $result;
+    }
+
+    /**************************************************************************
      * BINARY FUNCTIONS
      **************************************************************************/
 
@@ -394,61 +449,6 @@ class ArbitraryInteger implements ObjectArithmetic
         return [$int, $mod];
     }
 
-    /**************************************************************************
-     * UNARY FUNCTIONS
-     **************************************************************************/
-
-    /**
-     * Multiply by -1
-     *
-     * If $this is zero, then do nothing
-     */
-    public function negate(): ArbitraryInteger
-    {
-        $result = new ArbitraryInteger(0);
-        $base256 = $this->base256;
-        $result->setVariables($base256, $base256 == chr(0) ? true : !$this->positive);
-        return $result;
-    }
-
-    /**
-     * Integer Square Root
-     *
-     * Calculate the largest integer less than the square root of an integer.
-     * https://en.wikipedia.org/wiki/Integer_square_root
-     */
-    public function isqrt(): ArbitraryInteger
-    {
-        $length = strlen($this->base256);
-        // Start close to the value, at a number around half the digits.
-        $X = self::fromBinary(substr($this->base256, 0, intdiv($length, 2) + 1), true);
-        $lastX = $X;
-        $converge = false;
-        while (!$converge) {
-            list($NX, $mod) = $this->intdiv($X);
-            list($X, $mod) = $X->add($NX)->intdiv(2);
-            if ($X->equals($lastX) || $X->equals($lastX->add(1))) {
-                $converge = true;
-            } else {
-                $lastX = $X;
-            }
-        }
-        return $lastX;
-    }
-
-    /**
-     * Absolute Value
-     *
-     * @return ArbitraryInteger
-     */
-    public function abs(): ArbitraryInteger
-    {
-        $result = new ArbitraryInteger(0);
-        $base256 = $this->base256;
-        $result->setVariables($base256, true);
-        return $result;
-    }
-
     /**
      * Factorial
      *
@@ -468,37 +468,6 @@ class ArbitraryInteger implements ObjectArithmetic
             $result = $result->multiply($i_obj);
         }
         return $result;
-    }
-
-    /**
-     * Ackermann Function
-     * A well known highly recursive function which produces very large numbers
-     *
-     * https://en.wikipedia.org/wiki/Ackermann_function
-     *
-     * @param $m
-     * @param $n
-     * @return ArbitraryInteger
-     */
-    public static function ackermann($m, $n): ArbitraryInteger
-    {
-        $m = self::prepareParameter($m);
-        $n = self::prepareParameter($n);
-        if ($m->equals(0)) {
-            return $n->add(1);
-        } elseif ($m->equals(1)) {
-            return $n->add(2);
-        } elseif ($m->equals(2)) {
-            return $n->leftShift(1)->add(3);
-        } elseif ($m->equals(3)) {
-            $one = new ArbitraryInteger(1);
-            // 2^(n+3) - 3
-            return $one->leftShift($n->add(3))->subtract(3);
-        } elseif ($n->equals(0)) {
-            return ArbitraryInteger::ackermann($m->subtract(1), 1);
-        } else {
-            return ArbitraryInteger::ackermann($m->subtract(1), ArbitraryInteger::ackermann($m, $n->subtract(1)));
-        }
     }
 
     /**************************************************************************
