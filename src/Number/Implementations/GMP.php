36ebcb4304ecd75358bc306a4df739b0d97d0baf
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-11-22 16:37:45 -0500

    Create GMP.php

---
+++ src/Number/Implementations/GMP.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace MathPHP\Number\Implementations;
+
+use MathPHP\Number\ObjectArithmetic;
+
+class GMP implements ObjectArithmetic
+{
+    public function __construct()
+    {
+    }
+
+    public function fact()
+    {
+    }
+
+    public function add($number)
+    {
+    }
+
+    public function subtract($number)
+    {
+    }
+
+    public function multiply($number)
+    {
+    }
+
+    public function intdiv($number)
+    {
+    }
+
+    public function mod($number)
+    {
+    }
+
+    public function pow($number)
+    {
+    }
+
+    public function equals($number)
+    {
+    }
+
+    public function greaterThan($number)
+    {
+    }
+
+    public function lessThan($number)
+    {
+    }
+}
