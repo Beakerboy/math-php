475f3701fcc3e6cc987f07c7b3c7520b4241a381
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-11-22 16:30:09 -0500

    Create ArbitraryInteger.php

---
+++ src/Number/ArbitraryInteger.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace MathPHP\Number;
+
+class ArbitraryInteger implements ObjectArithmetic
+{
+
+    private $object;
+
+    public function __construct($int)
+    {
+        if (is_object($int)) {
+            $this->object = $int;
+        } else {
+            $this->object = new implementations\NativePHP($int)
+        }
+    }
+
+    public function __toString()
+    {
+        return $this->object->__toString();
+    }
+
+    public function fact()
+    {
+        return new ArbitraryInteger($this->object->fact());
+    }
+
+    public function add($number)
+    {
+        return new ArbitraryInteger($this->object->add($number));
+    }
+
+    public function subtract($number)
+    {
+        return new ArbitraryInteger($this->object->subtract($number));
+    }
+
+    public function multiply($number)
+    {
+        return new ArbitraryInteger($this->object->multiply($number));
+    }
+
+    public function intdiv($number)
+    {
+        return new ArbitraryInteger($this->object->divide($number));
+    }
+
+    public function mod($number)
+    {
+        return new ArbitraryInteger($this->object->mod($number));
+    }
+
+    public function pow($number)
+    {
+        return new ArbitraryInteger($this->object->pow($number));
+
+    public function equals($number)
+    {
+        return $this->object->equals($number);
+    }
+
+    public function lessThan($number)
+    {
+        return $this->object->lessThan($number);
+    }
+
+    public function greaterThan($number)
+    {
+        return $this->object->greaterThan($number);
+    }
+}
