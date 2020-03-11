b16f3972380ca3ac384970c2082d0e19e8830e12
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-03-10 22:23:38 -0400

    Update HypergeometricTest.php

--- tests/Probability/Distribution/Multivariate/HypergeometricTest.php
+++ tests/Probability/Distribution/Multivariate/HypergeometricTest.php
@@ -11,4 +11,28 @@ class HypergeometricTest extends \PHPUnit\Framework\TestCase
         $dist = new Hypergeometric([15, 10, 15]);
         $this->assertEquals(496125 / 3838380, $dist->pmf([2, 2, 2]));
     }
+
+    /**
+     * @dataProvider dataProviderForConstructorExceptions
+     */
+    public function testConstructorException($quantities)
+    {
+        $this->expectException(BadDataException::class);
+        $dist = new Hypergeometric($quantities);
+    }
+
+    public function dataProviderForConstructorExceptions()
+    {
+        return [
+            'float' => [
+                [.5, 1, 6],
+            ],
+            'string' => [
+                [10, '1', 6],
+            ],
+            'less than one'=> [
+                [0, 1, 6],
+            ]
+        ];
+    }
 }
