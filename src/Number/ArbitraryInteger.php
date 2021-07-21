109dbef63a66ea7196f25c1b7ee17ba71282a0c2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2021-07-21 16:24:48 -0400

    Update ArbitraryInteger.php

--- src/Number/ArbitraryInteger.php
+++ src/Number/ArbitraryInteger.php
@@ -546,9 +546,7 @@ class ArbitraryInteger implements ObjectArithmetic
             $negative_result = !$negative_result;
             [$int, $mod] = $this->abs()->fullIntdiv($divisor);
             if ($negative_result) {
-                $int = $int->negate();
-                throw new Exception\IncorrectTypeException((string) $int);
-                $int = $int->add(-1);
+                $int = $int->negate()->subtract(1);
                 $mod = $mod->negate()->add($divisor);
                 return [$int, $mod];
             }
